e0939f7510f6cf682232f5808f5eae4a8e542d67
Prabath <prabath0061@gmail.com>
2025-05-22 15:59:33 +0530

    second commit testing netlify

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ export default function HomePage() {
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
          {"Sup Nerds, are you guyl ready to build some awesome stuff with Next.js?"}
+         This is the second page of the app directory. You can navigate to the first page by clicking the link below.
       </div>
     </main>
   );
